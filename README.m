494184496772a1ef8f6e970edc73ba931fc92e70
akirincich <akirincich@whoi.edu>
2016-08-16 09:50:18 -0400

    Update README.m

--- README.m
+++ README.m
@@ -1,8 +1,14 @@
-%README.txt
+%README.m
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % repository for imageFOL software updates
-% This code is designed to a better FOL processing algorithm in Matlab.  The Main script to start with is
+% This code is designed to provide a better FOL processing algorithm in Matlab. Details of the method 
+%  can be found in the companion paper, 
+%
+% "Improved Detection of the First Order Region for Direction Finding HF Radars
+%   using Image Processing Techniques"  by A. Kirincich, JOAT (submitted-2016)   
+%
+% The Main script to start with is
 %
 % codar_DP_imageFOLtest_07122016.m
 %
@@ -15,7 +21,8 @@
 %     - calls  pksfinder.m   from within
 %     - requires the image processing toolbox in your version of Matlab
 %  
-%  Anthony Kirincich
+%  08/15/2016
+%   Anthony Kirincich
 %   Woods Hole Oceanographic Inst.
 %   
 %
