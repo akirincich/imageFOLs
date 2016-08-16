d8edffcdb646782466328ac81fceca2cf1124fba
akirincich <akirincich@whoi.edu>
2016-08-16 09:47:33 -0400

    Update READ_ME_data.m

--- data/READ_ME_data.m
+++ data/READ_ME_data.m
@@ -1,14 +1,22 @@
 %READ_ME_data.m
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-
-
-
-
-
-
+%  The data files in this folder are provided as examples for the estimation of 
+%     the first order limits using the imageFOL method, as seen in the JOAT 
+%     manuscript (submitted) describing the methodology
+%
+%   Any use of these data, other than for testing the imageFOL methods using the example scripts
+%    provided here requires additional permission of the data set authors, as described here:
+%  
+%  CSS_LPWR_11_04_06_0315.cs4   A. Kirincich WHOI
+%  CSS_LPWR_11_07_19_2230.cs4   A. Kirincich WHOI
+%  CSS_LPWR_11_08_02_0645.cs4   A. Kirincich WHOI
+%  CSS_LPWR_14_08_06_1730.cs4   A. Kirincich WHOI
+%  CSS_NWTP_16_05_20_2300.cs4   A. Kirincich WHOI
+%  
+%  CSS_STV1_05_08_24_1620.cs    M. Kosro   OSU
+%  
+%  CSS_HATY_16_04_28_0030.cs4   H. Seim  UNC, M. Muglia NC CSI, and S. Haines UNC
+%
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
